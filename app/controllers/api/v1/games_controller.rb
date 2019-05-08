47e4cde0ef8f2d3ea08165696adba9050e8a80d8
Tommy Lau <tlau425@yahoo.com>
2019-05-08 14:15:32 -0400

    custom route

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,26 +1,31 @@
 class Api::V1::GamesController < ApplicationController
-    def index 
-        @games = Game.all 
+    def index
+        @games = Game.all
         render json: @games
     end
 
     def show
         user =  User.find(params[:id])
-        render json: user 
+        render json: user
     end
 
     def create
-        @user = User.create(username:params[:username])
-        
+        @user = User.find_or_create_by(username:params[:username])
+
         # create_or_find_by
-        @game = Game.create(difficulty: params[:difficulty], high_score: 0,level: 0, score: 0, timer: 0, user_id: @user.id)
+        # @game = Game.create(difficulty: params[:difficulty], high_score: 0,level: 0, score: 0, timer: 0, user_id: @user.id)
 
        render json: @user
     end
 
+    def newgame
+      @game = Game.create(difficulty: params[:difficulty], high_score: 0,level: 0, score: 0, timer: 0, user_id: @user.id)
+      render json: @game
+    end
+
     def update
         user =  User.find(params[:id])
-        render json: user 
+        render json: user
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :games
+      post "/game", to: "games#newgame"
     end
   end
 end
